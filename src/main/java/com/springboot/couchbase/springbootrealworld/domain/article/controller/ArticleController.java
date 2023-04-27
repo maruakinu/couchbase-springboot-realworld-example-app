e455703edbdb96f0790584c2a83aa10a8a4572f1
maruakinu <marloaquino080621@gmail.com>
2023-04-27 10:52:03 +0800

    Adding Comments to Every functions

--- src/main/java/com/springboot/couchbase/springbootrealworld/domain/article/controller/ArticleController.java
+++ src/main/java/com/springboot/couchbase/springbootrealworld/domain/article/controller/ArticleController.java
@@ -36,26 +36,31 @@ public class ArticleController {
 
 
 
+    //Adding an Article
     @PostMapping
     public ArticleDto.SingleArticle<ArticleDto> createArticle(@RequestBody ArticleDto.SingleArticle<ArticleDto> article, @AuthenticationPrincipal AuthUserDetails authUserDetails) {
         return new ArticleDto.SingleArticle<>(articleService.createArticle(article.getArticle(), authUserDetails));
     }
 
+    //Get an Article using slug
     @GetMapping("/{slug}")
     public ArticleDto.SingleArticle<ArticleDto> getArticle(@PathVariable String slug, @AuthenticationPrincipal AuthUserDetails authUserDetails) {
         return new ArticleDto.SingleArticle<>(articleService.getArticle(slug, authUserDetails));
     }
 
+    //Update an Article using slug
     @PutMapping("/{slug}")
     public ArticleDto.SingleArticle<ArticleDto> createArticle(@PathVariable String slug, @RequestBody ArticleDto.SingleArticle<ArticleDto.Update> article, @AuthenticationPrincipal AuthUserDetails authUserDetails) {
         return new ArticleDto.SingleArticle<>(articleService.updateArticle(slug, article.getArticle(), authUserDetails));
     }
 
+    //Delete an Article using slug
     @DeleteMapping("/{slug}")
     public void deleteArticle(@PathVariable String slug, @AuthenticationPrincipal AuthUserDetails authUserDetails) {
         articleService.deleteArticle(slug, authUserDetails);
     }
 
+    //Post a comment with a slug as parameter
     @PostMapping("/{slug}/comments")
     public CommentDto.SingleComment addCommentsToAnArticle(@PathVariable String slug, @RequestBody CommentDto.SingleComment comment, @AuthenticationPrincipal AuthUserDetails authUserDetails) {
         return CommentDto.SingleComment.builder()
@@ -63,23 +68,27 @@ public class ArticleController {
                 .build();
     }
 
-
+    //Get all comments to an article with a slug as parameter
     @GetMapping("/{slug}/comments")
     public CommentDto.MultipleComments getCommentsFromAnArticle(@PathVariable String slug, @AuthenticationPrincipal AuthUserDetails authUserDetails) {
         return CommentDto.MultipleComments.builder()
                 .comments(commentService.getCommentsBySlug(slug, authUserDetails))
                 .build();
     }
+
+    //delete a comment to an article with a comment id as parameter
     @DeleteMapping("/comments/{commentId}")
     public void deleteComment(@PathVariable("commentId") String commentId, @AuthenticationPrincipal AuthUserDetails authUserDetails) {
         commentService.delete(commentId, authUserDetails);
     }
 
+    //Post a favorite article with a slug as a parameter
     @PostMapping("/{slug}/favorite")
     public ArticleDto.SingleArticle<ArticleDto> favoriteArticle(@PathVariable String slug, @AuthenticationPrincipal AuthUserDetails authUserDetails) {
         return new ArticleDto.SingleArticle<>(articleService.favoriteArticle(slug, authUserDetails));
     }
 
+    //Get a favorite article with a slug as a parameter
     @GetMapping("/{slug}/favorites")
     public FavoriteDto.MultipleFavorites getFavoritesFromAnArticle(@PathVariable String slug, @AuthenticationPrincipal AuthUserDetails authUserDetails) {
         return FavoriteDto.MultipleFavorites.builder()
@@ -87,18 +96,19 @@ public class ArticleController {
                 .build();
     }
 
+    //Delete a favorite article with a slug as a parameter
     @DeleteMapping("/{slug}/favorite")
     public void deleteFavorite(@PathVariable("slug") String slug, @AuthenticationPrincipal AuthUserDetails authUserDetails) {
         favoriteService.delete(slug, authUserDetails);
     }
 
-
+    //Get all article with pagination and user authentication
     @GetMapping("/all")
     public ArticleDto.MultipleArticle feedArticles(@ModelAttribute @Valid FeedParams feedParams, @AuthenticationPrincipal AuthUserDetails authUserDetails) {
         return ArticleDto.MultipleArticle.builder().articles(articleService.feedArticles(authUserDetails, feedParams)).build();
     }
 
-
+    //Get all article with user authentication
     @GetMapping("/feed")
     public ArticleDto.MultipleArticle getArticles(@AuthenticationPrincipal AuthUserDetails authUserDetails) {
         return ArticleDto.MultipleArticle.builder()
@@ -106,6 +116,7 @@ public class ArticleController {
                 .build();
     }
 
+    //Get all article without user authentication
     @GetMapping
     public ArticleDto.MultipleArticle getArticlesYouFollow() {
         return ArticleDto.MultipleArticle.builder()
