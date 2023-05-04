bb106496417a3c3dc7c0bf48e14172e89f7c9e6b
maruakinu <marloaquino080621@gmail.com>
2023-05-04 14:02:31 +0800

    Refactoring of codes

--- src/main/java/com/springboot/couchbase/springbootrealworld/configuration/CouchbaseConfig.java
+++ src/main/java/com/springboot/couchbase/springbootrealworld/configuration/CouchbaseConfig.java
@@ -8,20 +8,6 @@ import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepos
 @EnableCouchbaseRepositories(basePackages={"com.springboot.couchbase.springbootrealworld"})
 public class CouchbaseConfig extends AbstractCouchbaseConfiguration {
 
-//    @Override
-//    protected void configureEnvironment(final ClusterEnvironment.Builder builder) {
-//        builder.securityConfig().enableTls(true);
-//                builder.ioConfig(IoConfig.enableDnsSrv(true)).build();
-//    }
-
-//    ClusterEnvironment env = ClusterEnvironment.builder()
-//            .securityConfig(SecurityConfig.enableTls(true)/*.trustManagerFactory(InsecureTrustManagerFactory.INSTANCE)*/)
-//            .ioConfig(IoConfig.enableDnsSrv(true)).build();
-
-//    public String getConnectionString() {
-//        return "cb.yy8kujgkkjbscew4.cloud.couchbase.com";
-//    };
-
     public String getConnectionString() {
         return "couchbase://127.0.0.1";
     };
